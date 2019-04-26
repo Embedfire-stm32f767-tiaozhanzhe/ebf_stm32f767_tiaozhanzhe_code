a968a1dd48732ee61060eabcefb6ddb311a7031c
zzz_xxj <995007786@qq.com>
2019-04-26 15:31:47 +0800

    修改I2C硬件配置宏定义

--- 13-I2C-读写EEPROM/User/i2c/bsp_i2c_ee.h
+++ 13-I2C-读写EEPROM/User/i2c/bsp_i2c_ee.h
@@ -9,31 +9,31 @@
 /* AT24C04/08A/16A每页有16个字节 */
 //#define EEPROM_PAGESIZE           16			
 
-#define EEPROM_I2C_FORCE_RESET()      __HAL_RCC_I2C4_FORCE_RESET()
-#define EEPROM_I2C_RELEASE_RESET()    __HAL_RCC_I2C4_RELEASE_RESET()
+#define EEPROM_I2C_FORCE_RESET()      __HAL_RCC_I2C1_FORCE_RESET()
+#define EEPROM_I2C_RELEASE_RESET()    __HAL_RCC_I2C1_RELEASE_RESET()
 /* I2C interrupt requests */
-#define EEPROM_I2C_EV_IRQn            I2C4_EV_IRQn
-#define EEPROM_I2C_ER_IRQn            I2C4_ER_IRQn
-#define EEPROM_I2C_EV_IRQHandler	  I2C4_EV_IRQHandler
-#define EEPROM_I2C_ER_IRQHandler	  I2C4_ER_IRQHandler
+#define EEPROM_I2C_EV_IRQn            I2C1_EV_IRQn
+#define EEPROM_I2C_ER_IRQn            I2C1_ER_IRQn
+#define EEPROM_I2C_EV_IRQHandler	  I2C1_EV_IRQHandler
+#define EEPROM_I2C_ER_IRQHandler	  I2C1_ER_IRQHandler
 
 /* 这个地址只要与STM32外挂的I2C器件地址不一样即可 */
 #define I2C_OWN_ADDRESS7      0X0A   
 
 /*I2C接口*/
-#define EEPROM_I2C                          I2C4
-#define EEPROM_I2C_CLK_ENABLE()             __HAL_RCC_I2C4_CLK_ENABLE()
-#define RCC_PERIPHCLK_I2Cx               	RCC_PERIPHCLK_I2C4
+#define EEPROM_I2C                          I2C1
+#define EEPROM_I2C_CLK_ENABLE()             __HAL_RCC_I2C1_CLK_ENABLE()
+#define RCC_PERIPHCLK_I2Cx               	RCC_PERIPHCLK_I2C1
 
-#define EEPROM_I2C_SCL_PIN                  GPIO_PIN_12                 
-#define EEPROM_I2C_SCL_GPIO_PORT            GPIOD                       
-#define EEPROM_I2C_SCL_GPIO_CLK_ENABLE()    __GPIOD_CLK_ENABLE()
-#define EEPROM_I2C_SCL_AF                   GPIO_AF4_I2C4
+#define EEPROM_I2C_SCL_PIN                  GPIO_PIN_6                 
+#define EEPROM_I2C_SCL_GPIO_PORT            GPIOB                       
+#define EEPROM_I2C_SCL_GPIO_CLK_ENABLE()    __GPIOB_CLK_ENABLE()
+#define EEPROM_I2C_SCL_AF                   GPIO_AF4_I2C1
 
-#define EEPROM_I2C_SDA_PIN                  GPIO_PIN_13                  
-#define EEPROM_I2C_SDA_GPIO_PORT            GPIOD                       
-#define EEPROM_I2C_SDA_GPIO_CLK_ENABLE()    __GPIOD_CLK_ENABLE()
-#define EEPROM_I2C_SDA_AF                   GPIO_AF4_I2C4
+#define EEPROM_I2C_SDA_PIN                  GPIO_PIN_7                  
+#define EEPROM_I2C_SDA_GPIO_PORT            GPIOB                       
+#define EEPROM_I2C_SDA_GPIO_CLK_ENABLE()    __GPIOB_CLK_ENABLE()
+#define EEPROM_I2C_SDA_AF                   GPIO_AF4_I2C1
 
 
 
