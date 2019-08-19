1369f73f46b8698c9a3f04a1ef8c8a658e5df4ee
LONG <1558193230@qq.com>
2019-08-19 14:48:48 +0800

    修改SDRAM配置文件，解决会进HardFault_Handler的BUG

--- 17-FMC—扩展外部SDRAM/User/sdram/bsp_sdram.c
+++ 17-FMC—扩展外部SDRAM/User/sdram/bsp_sdram.c
@@ -367,8 +367,8 @@ void SDRAM_Init(void)
   hsdram1.Instance = FMC_SDRAM_DEVICE;
   /* hsdram1.Init */
   hsdram1.Init.SDBank = FMC_SDRAM_BANK2;
-  hsdram1.Init.ColumnBitsNumber = FMC_SDRAM_COLUMN_BITS_NUM_8;
-  hsdram1.Init.RowBitsNumber = FMC_SDRAM_ROW_BITS_NUM_12;
+  hsdram1.Init.ColumnBitsNumber = FMC_SDRAM_COLUMN_BITS_NUM_9;
+  hsdram1.Init.RowBitsNumber = FMC_SDRAM_ROW_BITS_NUM_13;
   hsdram1.Init.MemoryDataWidth = FMC_SDRAM_MEM_BUS_WIDTH_16;
   hsdram1.Init.InternalBankNumber = FMC_SDRAM_INTERN_BANKS_NUM_4;
   hsdram1.Init.CASLatency = FMC_SDRAM_CAS_LATENCY_3;
